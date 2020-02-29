3c35d295b86f107f925a3e9ba7c8c7eca70f060c
Nick Wills <37917347+KingOfCaves@users.noreply.github.com>
2020-02-28 19:11:34 -0500

    adding albion remastered to portfolio, rerunning post

--- docs/scripts.bcf3243b.js
+++ docs/scripts.bcf3243b.js
@@ -121,103 +121,83 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 (function () {
   var data = {
     fullstack: {
-      heading: "Fullstack Development",
-      projects: [// {
-      // 	id: "chat-app",
-      // 	href: "https://github.com/KingOfCaves/wills-chat-app",
-      // 	link: "http://github//chat-app",
-      // 	title: "Chat App",
-      // 	desc: 'A fullstack chat application with a Node.js backend. Uses socket.io to emit and detect events in real time.'
-      // },
-      // {
-      // 	id: "weather-app",
-      // 	href: "https://github.com/KingOfCaves/wills-weather-app",
-      // 	link: "http://github//weather-app",
-      // 	title: "Weather App",
-      // 	desc: 'A simple weather fetching web application. Uses Node.js to access the DarkSky API and Mapbox API in the background.'
-      // },
-      {
-        id: "fountain-of-dreams",
-        href: "https://github.com/KingOfCaves/fountain-of-dreams",
-        link: "http://github//fountain-of-dreams",
-        title: "Fountain of Dreams",
-        desc: 'A mallsoft/vaporwave oriented live radio that\'s currently live now! An ongoing project that will continue to be added to and perfected.'
+      heading: 'Fullstack Development',
+      projects: [{
+        id: 'fountain-of-dreams',
+        href: 'https://github.com/KingOfCaves/fountain-of-dreams',
+        link: 'http://github//fountain-of-dreams',
+        title: 'Fountain of Dreams',
+        desc: "A mallsoft/vaporwave oriented live radio that's currently live now! An ongoing project that will continue to be added to and perfected."
       }]
     },
     frontend: {
-      heading: "Frontend Development",
+      heading: 'Frontend Development',
       projects: [{
-        id: "portfolio-project",
-        href: "https://github.com/KingOfCaves/wills-portfolio-project",
-        link: "http://github//portfolio-project",
-        title: "Portfolio Project",
-        desc: 'A portfolio based on a design provided by Udacity. By using html and css you recreate the design from the ground up, adding your own touches.'
+        id: 'albion-remastered',
+        href: 'https://github.com/KingOfCaves/albion-remastered',
+        link: 'http://github//albion-remastered',
+        title: 'Albion Remastered',
+        desc: "Reacreating Albion Sword's website into a React.js powered single page application. The app takes the original and applies multiple programmatic and design changes that improve the overall user experience. Uses React Router and React."
       }, {
-        id: "memory-game",
-        href: "https://github.com/KingOfCaves/wills-memory-game",
-        link: "http://github//memory-game",
-        title: "Memory Game",
-        desc: 'A simple memory game that uses nothing but vanilla JavaScript, HTML and CSS. Made to look good on every device!'
-      }, {
-        id: "arcade-classic",
-        href: "https://github.com/KingOfCaves/wills-arcade-classic",
-        link: "http://github//arcade-classic",
-        title: "Arcade Classic",
-        desc: 'Recreation of a popular classic arcade game, Frogger. Uses canvas, ES6 classes, sprite images, and an engine provided by Udacity.'
-      }, {
-        id: "feedreader-testing",
-        href: "https://github.com/KingOfCaves/wills-feedreader-testing",
-        link: "http://github//feedreader-testing",
-        title: "Feedreader Testing",
-        desc: 'A suite built for testing the functionality of another person\'s feedreader application. Built using Jasmine.js.'
-      }, {
-        id: "restaurant-review",
-        href: "https://github.com/KingOfCaves/wills-restaurant-review",
-        link: "http://github//restaurant-review",
-        title: "Restaurant Review App",
-        desc: 'Troubleshooting a restaurant review app by adding aria labeling for accessibility, adding necessary headers to the mapbox api and making the app compatible with all devices.'
-      }, {
-        id: "the-big-brown",
-        href: "https://github.com/KingOfCaves/the-big-brown-frontend",
-        link: "http://github//the-big-brown",
-        title: "The Big Brown",
+        id: 'the-big-brown',
+        href: 'https://github.com/KingOfCaves/the-big-brown-frontend',
+        link: 'http://github//the-big-brown',
+        title: 'The Big Brown',
         desc: 'Frontend for a fake restaurant website. Uses Parcel for compiling all of the scripts, scss files and html templates. Also uses PostCSS and PostHTML for utilizing autoprefixer and other related plugins.'
       }, {
-        id: "jamila-wallace-portfolio",
-        href: "https://github.com/KingOfCaves/jamila-wallace-portfolio",
-        link: "http://github//j.-wallace-portfolio",
+        id: 'jamila-wallace-portfolio',
+        href: 'https://github.com/KingOfCaves/jamila-wallace-portfolio',
+        link: 'http://github//j.-wallace-portfolio',
         title: "J. Wallace's Portfolio",
-        desc: 'A recreation of my client\'s design. By using my client\'s Adobe XD design as a base, a near 1:1 responsive reconstruction was made in HTML, CSS and JavaScript.'
+        desc: "A recreation of my client's design. By using my client's Adobe XD design as a base, a near 1:1 responsive reconstruction was made in HTML, CSS and JavaScript."
+      }, {
+        id: 'portfolio-project',
+        href: 'https://github.com/KingOfCaves/wills-portfolio-project',
+        link: 'http://github//portfolio-project',
+        title: 'Portfolio Project',
+        desc: 'A portfolio based on a design provided by Udacity. By using html and css you recreate the design from the ground up, adding your own touches.'
+      }, {
+        id: 'memory-game',
+        href: 'https://github.com/KingOfCaves/wills-memory-game',
+        link: 'http://github//memory-game',
+        title: 'Memory Game',
+        desc: 'A simple memory game that uses nothing but vanilla JavaScript, HTML and CSS. Made to look good on every device!'
+      }, {
+        id: 'arcade-classic',
+        href: 'https://github.com/KingOfCaves/wills-arcade-classic',
+        link: 'http://github//arcade-classic',
+        title: 'Arcade Classic',
+        desc: 'Recreation of a popular classic arcade game, Frogger. Uses canvas, ES6 classes, sprite images, and an engine provided by Udacity.'
+      }, {
+        id: 'feedreader-testing',
+        href: 'https://github.com/KingOfCaves/wills-feedreader-testing',
+        link: 'http://github//feedreader-testing',
+        title: 'Feedreader Testing',
+        desc: "A suite built for testing the functionality of another person's feedreader application. Built using Jasmine.js."
+      }, {
+        id: 'restaurant-review',
+        href: 'https://github.com/KingOfCaves/wills-restaurant-review',
+        link: 'http://github//restaurant-review',
+        title: 'Restaurant Review App',
+        desc: 'Troubleshooting a restaurant review app by adding aria labeling for accessibility, adding necessary headers to the mapbox api and making the app compatible with all devices.'
       }]
-    } // backend: {
-    // 	heading: "Backend Development",
-    // 	projects: [
-    // 		{
-    // 			id: "task-manager-api",
-    // 			href: "https://github.com/KingOfCaves/wills-task-manager-api",
-    // 			link: "http://github//task-manager-api",
-    // 			title: "Task Manager API",
-    // 			desc: 'An API developed using Node.js. The API comes with several sophisticated features such as an authentication middleware, CRUD endpoints and image manipulation.'
-    // 		}
-    // 	]
-    // }
-
+    }
   };
   var $projects = document.querySelector('.main');
-  var compiledHTML = "";
+  var compiledHTML = '';
 
   for (x in data) {
     var _data$x = data[x],
         heading = _data$x.heading,
         projects = _data$x.projects;
-    var projectHTML = "".trim();
+    var projectHTML = ''.trim();
 
     try {
       projects.forEach(function (project) {
         projectHTML += "\n\t\t\t\t<div class=\"project\">\n\t\t\t\t\t<a class=\"project__graphic\" href=\"".concat(project.href, "\" target=\"_blank\" id=\"").concat(project.id, "\">\n\t\t\t\t\t\t<p class=\"project__link\">").concat(project.link, "</p>\n\t\t\t\t\t</a>\n\t\t\t\t\t<p class=\"project__title\">").concat(project.title, "</p>\n\t\t\t\t\t<p class=\"project__desc\">").concat(project.desc, "</p>\n\t\t\t\t</div>").trim();
       });
     } catch (error) {
-      console.log('Something wen\'t wrong.', error);
+      console.log("Something wen't wrong.", error);
     }
 
     var typeHTML = "\n\t\t<div class=\"projects\">\n\t\t\t<h2 class=\"heading-secondary\">".concat(heading, "</h2>\n\t\t\t").concat(projectHTML, "\n\t\t</div>").trim();
@@ -254,7 +234,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58309" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57968" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
